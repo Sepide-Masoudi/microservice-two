9372084e4b0f8792f87247536f2bb49612599072
sepideh.masoudi <sepideh.masoudi@tu-berlin.de>
2024-10-28 11:32:02 +0100

    microservice-two

--- src/test/java/com/tub/ise/MicroserviceTwoApplicationTests.java
+++ src/test/java/com/tub/ise/MicroserviceTwoApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class IseMicroserviceApplicationTests {
+class MicroserviceTwoApplicationTests {
 
 	@Test
 	void contextLoads() {
